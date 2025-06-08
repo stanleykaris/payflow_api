0f9cced79ef1e91a6d8d1b57425922db6aa364ff
Stanley Kariuki <stanleykariuki1999@gmail.com>
2025-06-08 16:38:29 +0300

    fix(settings): set DEBUG to False and allow all hosts in production

--- payflow/payflow/settings.py
+++ payflow/payflow/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^p8j=s24obiok=y+-7c(869vmxxo8jvz=pj^sqppps#t+#l1c8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
